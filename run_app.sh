1015022b6a84e5d74e835a0ce89b144817d5da40
Kent Emman <38069509+Kentemman@users.noreply.github.com>
2023-03-22 21:34:29 +0800

    Update run_app.sh

--- run_app.sh
+++ run_app.sh
@@ -14,23 +14,9 @@ sudo apt-get install python3-pip
 # Install dependencies from requirements.txt
 sudo pip3 install -r requirements.txt
 
-# Run app.py
-python3 app.py
-#!/bin/bash
 
-# Update Ubuntu
 
-sudo apt-get update
 
-sudo apt-get upgrade
-
-# Install pip
-
-sudo apt-get install python3-pip
-
-# Install dependencies from requirements.txt
-
-sudo pip install -r requirements.txt
 
 # Prompt user to enter OpenAI API key
 
